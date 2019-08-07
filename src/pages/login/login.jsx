a8a37c61f8a07aff5ec4704e1f37d21973fc1bf6
Emma Liu <helveticrolls79@gmail.com>
2019-08-07 23:11:47 +0900

    finish the static login component

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Form, Icon, Input, Button} from 'antd';
 
 import './login.less'
 import logo from './images/logo.png'
@@ -8,6 +9,10 @@ login router component
 */
 
 export default class Login extends Component {
+
+    handleSubmit = (event) => {
+
+    }
     render() {
         return (
             <div className="login">
@@ -17,7 +22,26 @@ export default class Login extends Component {
                 </header>
                 <section className="login-content">
                     <h2>Login In</h2>
-                    <div>组件标签</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        </Form.Item>
+                    </Form>
                 </section>
             </div>
         )
