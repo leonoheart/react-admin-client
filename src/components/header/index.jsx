adad436758d09e796bcf04171cce2f255644ef62
Emma <emma@MacBook-Pro-2.local>
2019-08-26 23:12:04 +0900

    add header-top

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,11 @@ export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                leftN
+                <div className="header-top">
+                    <span>ようこそ、admin</span>
+                    <a href="javascript:">ログアウト</a>
+                </div>
+                <div className="header-bottom"></div>
             </div>
         )
     }
